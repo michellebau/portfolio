9b19f4b55e3f26a0e528f545db3acdbf91281065
michellebau <aermichelle@gmail.com>
2024-04-05 16:21:50 -0400

    fixed broken links

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -15,10 +15,10 @@ const ContactSection = () => {
                     I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
                 </p>
                 <div className="socials flex flex-row gap-2">
-                    <Link href="github.com/michellebau" target="_blank">
+                    <Link href="https://github.com/michellebau" target="_blank">
                         <Image src={GitHubIcon} alt="GitHub Icon"></Image>
                     </Link>
-                    <Link href="linkedin.com/in/michelleabautista" target="_blank">
+                    <Link href="https://www.linkedin.com/in/michelleabautista/" target="_blank">
                         <Image src={LinkedInIcon} alt="LinkedIn icon"></Image>
                     </Link>
                 </div>
