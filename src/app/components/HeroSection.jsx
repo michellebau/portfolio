f57b919539bee7a89e754fff7335149250222370
michellebau <aermichelle@gmail.com>
2024-04-05 16:17:58 -0400

    fixed missed apostrophe

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
               />
             </h1>
             <p className="text-[#fff] text-base sm:text-lg mb-6 lg:text-xl">
-              I'm a May 2024 graduate seeking opportunities in web development or data analysis.
+              I&apos;m a May 2024 graduate seeking opportunities in web development or data analysis.
             </p>
             <div className="flex flex-col sm:flex-row justify-center sm:justify-start items-center sm:items-start gap-2">
               <Link
